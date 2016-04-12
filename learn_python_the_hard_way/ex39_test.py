73ea5aefa1ef0e305d2df8d98f4512a0d66cfce5
Fan Chun <fanchun@pku.edu.cn>
2016-04-12 09:04:12 +0800

    new file:   learn_python_the_hard_way/ex39_test.py new file:   learn_python_the_hard_way/hashmap.py

---
+++ learn_python_the_hard_way/ex39_test.py
@@ -0,0 +1,8 @@
+import hashmap
+
+# create a mapping of state to abbreviation
+states = hashmap.new()
+hashmap.set(states, 'Oregon', 'OR')
+hashmap.set(states, 'Florida', 'FL')
+hashmap.set(states, 'California', 'CA')
+hashmap.set(states, 'New Yourk', 'NY')
